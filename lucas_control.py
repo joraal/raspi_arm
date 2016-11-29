a17686348e332437256d4b366db2fb20ef9ba109
lucas <joraal@gmail.com>
2016-11-29 08:28:10 +0100

    added gui and multiservo

--- lucas_control.py
+++ lucas_control.py
@@ -8,6 +8,8 @@ import time
 # Import the PCA9685 module.
 import Adafruit_PCA9685
 
+from Tkinter import *
+
 
 # Uncomment to enable debug output.
 #import logging
@@ -37,10 +39,78 @@ def set_servo_pulse(channel, pulse):
 # Set frequency to 60hz, good for servos.
 pwm.set_pwm_freq(60)
 
-print('Moving servo on channel 0, press Ctrl-C to quit...')
-while True:
+
+# define the gui
+
+
+class App:
+	def __init__(self, master):
+		frame = Frame(master)
+		frame.pack()
+		
+		scale1 = Scale(frame, from_=0, to=180,
+		orient=HORIZONTAL, command=self.update1)
+		scale1.grid(row=0)
+
+		scale2 = Scale(frame, from_=0, to=180,
+		orient=HORIZONTAL, command=self.update2)
+		scale2.grid(row=0)
+
+		scale3 = Scale(frame, from_=0, to=180,
+		orient=HORIZONTAL, command=self.update3)
+		scale3.grid(row=0)
+		
+		scale4 = Scale(frame, from_=0, to=180,
+		orient=HORIZONTAL, command=self.update4)
+		scale4.grid(row=0)
+		
+		scal5 = Scale(frame, from_=0, to=180,
+		orient=HORIZONTAL, command=self.update5)
+		scale5.grid(row=0)
+		
+		scal6 = Scale(frame, from_=0, to=180,
+		orient=HORIZONTAL, command=self.update6)
+		scal6.grid(row=0)
+		
+	def update1(self, angle):
+		pulse_len = int(float(angle) * 500.0 / 180.0) + 110
+		pwm.set_pwm(0, 0, pulse_len)
+
+
+	def update2(self, angle):
+		pulse_len = int(float(angle) * 500.0 / 180.0) + 110
+		pwm.set_pwm(1, 0, pulse_len)
+		
+	def update3(self, angle):
+		pulse_len = int(float(angle) * 500.0 / 180.0) + 110
+		pwm.set_pwm(2, 0, pulse_len)
+		
+	def update4(self, angle):
+		pulse_len = int(float(angle) * 500.0 / 180.0) + 110
+		pwm.set_pwm(3, 0, pulse_len)
+		
+	def update5(self, angle):
+		pulse_len = int(float(angle) * 500.0 / 180.0) + 110
+		pwm.set_pwm(4, 0, pulse_len)
+		
+	def updat6(self, angle):
+		pulse_len = int(float(angle) * 500.0 / 180.0) + 110
+		pwm.set_pwm(5, 0, pulse_len)
+		
+		
+
+root = Tk()
+root.wm_title('Servo Control')
+app = App(root)
+root.geometry("200x50+0+0")
+root.mainloop()
+
+
+
+#print('Moving servo on channel 0, press Ctrl-C to quit...')
+#while True:
     # Move servo on channel O between extremes.
-    pwm.set_pwm(0, 0, servo_min)
-    time.sleep(1)
-    pwm.set_pwm(0, 0, servo_max)
-    time.sleep(1)
+ #   pwm.set_pwm(0, 0, servo_min)
+  #  time.sleep(1)
+   # pwm.set_pwm(0, 0, servo_max)
+    #time.sleep(1)
